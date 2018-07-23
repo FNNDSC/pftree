6d518d3b60e5a9826d04ac01ef3e9515c4f34ff8
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2018-07-23 00:07:40 -0400

    Bump to 1.4.12

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pftree',
-      version          =   '1.4.10',
+      version          =   '1.4.12',
       description      =   '(Python) utility to create dict representations of file system trees.',
       long_description =   readme(),
       author           =   'FNNDSC',
