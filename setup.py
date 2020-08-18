3aedb85f2d4820af54bd91e9f7a54bb8bb08ee3f
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2020-08-18 12:18:09 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pftree',
-      version          =   '2.0.4',
+      version          =   '2.1.0',
       description      =   'Utility to create dict representations of file system trees.',
       long_description =   readme(),
       author           =   'FNNDSC',
