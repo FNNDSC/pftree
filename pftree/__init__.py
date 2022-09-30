2d1fd43a1af8d2985bb433cff5fd11fc62a368da
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2022-09-30 13:58:15 -0400

    Update with new design pattern

--- pftree/__init__.py
+++ pftree/__init__.py
@@ -1,4 +1,10 @@
-try:
-    from .pftree    import pftree
-except:
-    from pftree     import pftree
+from importlib.metadata import Distribution
+
+__pkg = Distribution.from_name(__package__)
+__version__ = __pkg.version
+
+
+# try:
+#     from .pftree    import pftree
+# except:
+#     from pftree     import pftree
